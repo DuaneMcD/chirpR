57fb139dfd826a1af4bd7c58ca83cb2d37b5e1ca
Duane McDonald <mcdonald.duane13@gmail.com>
2021-05-12 22:35:26 -0400

    WIP tweet card

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
+import Card from './components/card';
+import Navbar from './components/Navbar';
+import freedom from './images/freedom.gif';
+import birdGif from './images/birdGif.gif';
 
 function App() {
-  return <div className='App'></div>;
+  const testTweet = 'Imma make it Tweet! 🐔🦚';
+
+  return (
+    <div className='App'>
+      <Navbar />
+      <div className='cardContainer'>
+        <Card
+          className='card'
+          avatar={birdGif}
+          handle='bigTweetr'
+          tweet={testTweet}
+          media={freedom}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
