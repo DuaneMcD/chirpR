8053ce18855e5e4d42f332543250505456c7c37a
Duane McDonald <mcdonald.duane13@gmail.com>
2021-06-14 23:43:57 -0400

    WIP favorites Array

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,40 +1,29 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Accordion from '../components/Accordion';
-import { CaretDownFilled } from '@ant-design/icons';
 import './home.css';
 
 const Home = () => {
+  const [favoriteIDs, setFavoriteIDs] = useState([
+    // { user: ' elonmusk', id: 44196397 },
+    { user: 'theellenshow', id: 15846407 },
+  ]);
   const [tweetsArray, setTweetsArray] = useState([]);
   const [includesArray, setIncludesArray] = useState([]);
-  const [active, setActive] = useState([]);
-  const [display, setDisplay] = useState('none');
-  const contentRef = useRef(null);
 
   const fetchUserTweets = async () => {
-    // const response = await fetch(`http://localhost:3000/timeline/${userId}`);
-    const response = await fetch(`http://localhost:3000/timeline/44196397`);
-    const message = await response.json();
-    setTweetsArray(await message.data);
-    setIncludesArray(await message.includes);
+    favoriteIDs.map(async user => {
+      const response = await fetch(`http://localhost:3000/timeline/${user.id}`);
+      // const response = await fetch(`http://localhost:3000/timeline/44196397`);
+      const message = await response.json();
+      setTweetsArray(await [message.data]);
+      setIncludesArray(await message.includes);
+    });
   };
 
   useEffect(() => {
     fetchUserTweets();
   }, []);
-
-  const toggleAccordion = () => {
-    setActive(!active);
-    setDisplay(active ? 'inherit' : 'none');
-  };
-
-  const text = [
-    {
-      text: `A dog is a type of domesticated animal. Known for its loyalty and
-      faithfulness, it can be found as a welcome guest in many households across
-      the world.`,
-    },
-  ];
 
   return (
     <>
@@ -42,8 +31,12 @@ const Home = () => {
       <div className='homePage'>
         <div className='content-box'>
           <h1>Popular Twitter Streams:</h1>
-          <Accordion title='@ElonMusk' userStream={tweetsArray} />
-          <Accordion title='@TheRealSammy' userStream={text} />
+          {tweetsArray.map((tweet, index) => (
+            <Accordion
+              title={`@${favoriteIDs[index]?.user}`}
+              userStream={tweet}
+            />
+          ))}
         </div>
       </div>
     </>
