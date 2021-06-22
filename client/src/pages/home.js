7ed719682a94af0c7a5e5d558bdf767a0be89ca3
Duane McDonald <mcdonald.duane13@gmail.com>
2021-06-21 22:09:02 -0400

    accordion handles solved

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -13,6 +13,7 @@ const Home = () => {
   ]);
   const [tweetsArray, setTweetsArray] = useState([]);
   const [includesArray, setIncludesArray] = useState([]);
+  const [usernameArray, setUsernameArray] = useState([]);
   const tweets = [];
   let user = [];
 
@@ -26,15 +27,29 @@ const Home = () => {
     });
   };
 
+  const handleUserData = async () => {
+    let twitterHandles = [];
+    for (let i = 0; i < tweetsArray.length; i++) {
+      const twitHandle = tweetsArray[i];
+      let handle = await getUserInfo(twitHandle[0]?.author_id);
+      twitterHandles.push(handle);
+    }
+    setUsernameArray(twitterHandles);
+  };
+
   const getUserInfo = async id => {
     const response = await fetch(`http://localhost:3000/idlookup/${id}`);
     const message = await response.json();
-    return await message.data.name;
+    return await message.data.username;
   };
 
   useEffect(() => {
     fetchUserTweets();
   }, []);
+
+  useEffect(() => {
+    handleUserData();
+  }, [tweetsArray]);
 
   return (
     <>
@@ -43,9 +58,14 @@ const Home = () => {
           <h1 className='popular-streams'>Explore Trending Twitter Feeds</h1>
         </div>
         <div className='homeContainer'>
-          {tweetsArray.map((tweet, index) => (
-            <Accordion title={`@${tweet[0]?.author_id}`} userStream={tweet} />
-          ))}
+          {tweetsArray.map((tweet, index) => {
+            return (
+              <Accordion
+                title={`@ ${usernameArray[index]}`}
+                userStream={tweet}
+              />
+            );
+          })}
         </div>
       </div>
     </>
