12abb0ad11fbbfde4ee8e1f5546bd287d5c387ec
Duane McDonald <mcdonald.duane13@gmail.com>
2021-06-23 15:22:06 -0400

    handle Moz scrollbars. npm start prodction cmd

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
 app.use(cors());
-const port = 3000;
+const port = process.env.PORT || 3000;
 const token =
   'AAAAAAAAAAAAAAAAAAAAAHwhPgEAAAAA1UgvX4LIPR7m2ornJszRmD4iJ3c%3DYzQIu3Henyxr7TcvlRfkKZ14ZzFOxd5LJ7C10nZfmFKdzBIBbg';
 const config = {
