16ca316e20d05cb5b53e2e4ec81cd498c2a79b7f
Duane McDonald <mcdonald.duane13@gmail.com>
2022-06-16 08:24:28 -0400

    filenames

--- components/layout.js
+++ components/layout.js
@@ -3,6 +3,7 @@ import Sidebar from './Sidebar';
 export default function Layout({ children }) {
   return (
     <>
+      {/* /on all pages */}
       <Sidebar />
       <main>{children}</main>
     </>
